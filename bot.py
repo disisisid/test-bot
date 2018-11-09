480cd3bc4e7954677737ffc1b4b6438e5a5bb67e
disisisid <sidnshady@gmail.com>
2018-11-10 03:31:43 +0530

    autorole test

--- bot.py
+++ bot.py
@@ -11,6 +11,11 @@ async def on_ready():
 	print('Bot is ready!')
 
 @client.event
+async def on_member_join(member):
+	role = discord.utils.get(member.server.roles, name='cat')
+	await client.add.roles(member, role)
+
+@client.event
 async def on_message(message):
 	channel = message.channel
 	if message.content.startswith('.ping'):
